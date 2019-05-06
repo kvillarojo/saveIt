7afd33df605d6970a1ebe529aaf5a7ebebd69715
kvillarojo <kim.villarojo@gmail.com>
2019-05-06 17:14:48 +0800

    buget ui v1

--- src/components/pages/budget/Index.vue
+++ src/components/pages/budget/Index.vue
@@ -18,41 +18,52 @@
     >
       <v-icon>add</v-icon>
     </v-btn>
+    <v-container fluid grid-list-md>
+      <v-data-iterator
+        :items="categories"
+        :rows-per-page-items="rowsPerPageItems"
+        :pagination.sync="pagination"
+        content-tag="v-layout"
+        row
+        wrap
+      >
+        <template v-slot:item="props">
+          <v-flex
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card>
+               <v-toolbar color="blue" dark>
+                <v-card-title><h4>{{ props.item.name }}</h4>
+                </v-card-title>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon> settings </v-icon>
+                </v-btn>
+              </v-toolbar>
 
-    <v-container frid-list>
-      <!-- v-for="value in object" -->
-      <v-layout row>
-        <v-flex xs3 sm3 v-for="category in categories">
-          <v-card>
-            <v-toolbar color="pink" dark>
-              <v-toolbar-title>{{ category.name }}</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon> settings </v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-list two-line>
-              <template v-for="(item, index) in itemsCategory">
-                <v-list-tile :key="index" avatar ripple @click="">
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
-                    <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                  <v-list-tile-action>
-                    <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
-                    <v-icon color="grey lighten-1">star_border</v-icon>
-                  </v-list-tile-action>
+              <v-divider></v-divider>
+              <v-list dense>
+                <v-list-tile>
+                  <v-list-tile-content> Amount : </v-list-tile-content>
+                  <v-list-tile-content class="align-end" > ₱ {{ props.item.amount }}</v-list-tile-content>
                 </v-list-tile>
-                <v-divider v-if="index + 1 < items.length" :key="`divider-${index}`"></v-divider>
-              </template>
-            </v-list>
-          </v-card>
-        </v-flex>
-      </v-layout>
-
+                <v-list-tile>
+                  <v-list-tile-content> Expenses : </v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.expenses }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content> Balance : </v-list-tile-content>
+                  <v-list-tile-content class="align-end" >{{ props.item.ballace }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </template>
+      </v-data-iterator>
     </v-container>
-
     <v-dialog v-model="dialog" width="500px">
       <v-card>
         <v-card-title class="black lighten-4 py-4 title">
@@ -64,6 +75,12 @@
               <v-text-field v-model="name"
                 prepend-icon="storage"
                 placeholder="Categor Name"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field v-model="amount" type="number"
+                prepend-icon="monetization_on"
+                placeholder="Amount"
               ></v-text-field>
             </v-flex>
           </v-layout>
@@ -82,8 +99,13 @@
 <script>
 export default {
   data: () => ({
+    rowsPerPageItems: [4, 8, 12],
+    pagination: {
+        rowsPerPage: 4
+      },
     dialog: false,
     name: null,
+    amount: null,
     categories: [],
     items: [
         {
@@ -92,32 +114,30 @@ export default {
           to: '/'
         },
         {
-          text: 'Buget',
+          text: 'Bugets',
           disabled: true
         }
       ],
-    itemsCategory: [
-      { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-      { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-      { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-      { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-      { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' }
-    ]
+    itemsCategory: []
   }),
   watch: {
   },
   methods: {
     addCategory () {
-      this.categories.push({name: this.name})
+      this.categories.push({name: this.name, amount: this.amount})
       this.dialog = false
       this.name = ''
+      this.amount = ''
+      let countEl = 0
     }
   },
-
 }
 </script>
 <style lang="scss">
   .cat-mr {
     margin-right: 5px;
+  }
+  .v-card__title {
+      padding: 0px;
   }
 </style>
